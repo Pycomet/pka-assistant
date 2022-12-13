c0d4067fb663864fbb5c6912e698ee20a1509f08
Pycomet <alfredemmanuelinyang@gmail.com>
2022-12-13 15:16:56 +0100

    add google sheets

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from google.api_core.exceptions import PermissionDenied
+from google.oauth2.credentials import Credentials
+from googleapicliuent.discovery import build
 from models import User
 import asyncio
 import logging
@@ -10,6 +13,10 @@ from datetime import date
 from dotenv import load_dotenv
 load_dotenv()
 
+# Google sheet libraries
+credentials = Credentials.from_service_account_file('./keys.json')
+# Create a service object for interacting with the API
+service = build('sheets', 'v4', credentials=credentials)
 
 user = User
 LANGUAGE = user.language
@@ -24,11 +31,17 @@ logging.basicConfig(
 )
 
 TOKEN = os.getenv('TOKEN')
-API_ID = os.getnv('API_ID')
-API_HASH = os.getenv('API_HASH')
 
 DEBUG = True
 SERVER_URL = os.getenv("SERVER_URL")
 
 bot = telebot.TeleBot(token=TOKEN)
 app = Flask(__name__)
+
+
+# Use the service object to access the API and get the spreadsheet
+try:
+    spreadsheet = service.spreadsheets().get(
+        spreadsheetId='<spreadsheet_id>').execute()
+except PermissionDenied:
+    print("You do not have permission to access this spreadsheet")
